9cb0b731cdb6e2059c1bf68aa628ff091f07c8e9
杨永鑫 <1292924111@qq.com>
2022-09-16 10:09:08 +0800

    第三次初始化

--- src/main.ts
+++ src/main.ts
@@ -7,12 +7,17 @@ import zhCn from 'element-plus/es/locale/lang/zh-cn'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 /* 引入全局css */
 import './assets/main.css'
+// vueX
+import store from './store'
 
 
 const app = createApp(App)
 
 // 使用路由
 app.use(router)  
+
+// 使用vueX
+app.use(store)
 
 // 中文版本的elementPlus
 app.use(ElementPlus, {    
---
+++ src/store/index.ts
@@ -0,0 +1,9 @@
+
+import { createStore } from 'vuex'
+
+export default createStore({
+  // 模块引入
+  modules: {
+
+  }
+})
